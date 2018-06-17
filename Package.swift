fe224800cd210e69818f0e81a15c3dd6a409bbb5
viachpaliy <viachpaliy@ukr.net>
2018-06-17 10:28:23 +0300

    change repository from original to fork

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GIO",
     dependencies: [
-        .Package(url: "https://github.com/rhx/SwiftGObject.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/SwiftGObject.git", majorVersion: 2)
     ],
     swiftLanguageVersions: [3]
 )
