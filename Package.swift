c1580881a7b7f922d7e4b034f1f5869d55f24f25
viachpaliy <viachpaliy@ukr.net>
2018-06-17 14:32:06 +0300

    change version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GIO",
     dependencies: [
-        .Package(url: "https://github.com/viachpaliy/SwiftGObject.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/SwiftGObject.git", majorVersion: 1)
     ],
     swiftLanguageVersions: [3]
 )
